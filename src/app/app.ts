ae8c4f6b5a2963493e88f2b132971b4731d339dd
Tiến Trần <bamamicoffee@gmail.com>
2026-03-04 23:28:51 +0700

    feat: Update image gallery and font definitions
    Refactors the image gallery to include new coffee farm visuals and updates the Cormorant Garamond font definitions in the 404 page.

--- src/app/app.ts
+++ src/app/app.ts
@@ -26,16 +26,16 @@ export class App {
   }
 
   images = [
+    'https://lh3.googleusercontent.com/d/1Y-edHAeNOLGFE3RwklIB8Fa-sax03EUt',
+    'https://lh3.googleusercontent.com/d/1V9EDOPMlqLrVkUVLJQovd7OqUiqaRnTY',
     'https://lh3.googleusercontent.com/d/1UCATiFnS_niRS_NiZWwY9mtfKcmKNl2x',
     'https://lh3.googleusercontent.com/d/1V9EDOPMlqLrVkUVLJQovd7OqUiqaRnTY',
-    'https://lh3.googleusercontent.com/d/1Y-edHAeNOLGFE3RwklIB8Fa-sax03EUt',
-    'https://lh3.googleusercontent.com/d/1pP0dN-WU-WSp0S4nxPdC35MTAIkFEPuw',
     'https://lh3.googleusercontent.com/d/1Kk8J_czUsCI4KkYtjUFsOKdxcAqVddAQ',
     'https://lh3.googleusercontent.com/d/1QwkuOD6ZhBFat8QVpk_-QVvm6NPibwZC',
     'https://lh3.googleusercontent.com/d/13OiawCigdUzwXPGnTqNmBat-ypmzjJ0Y',
-    'https://lh3.googleusercontent.com/d/1QwkuOD6ZhBFat8QVpk_-QVvm6NPibwZC',
+    'https://lh3.googleusercontent.com/d/1V9EDOPMlqLrVkUVLJQovd7OqUiqaRnTY',
     'https://lh3.googleusercontent.com/d/13OiawCigdUzwXPGnTqNmBat-ypmzjJ0Y',
-    'https://lh3.googleusercontent.com/d/118dL5PVkIb04lOEk3v-yQZfJXxoDT3EV'
+    'https://lh3.googleusercontent.com/d/1UCATiFnS_niRS_NiZWwY9mtfKcmKNl2x'
   ];
 
   t(en: string, vi: string) {
